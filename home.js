ccff97ab0ff14c17840069fdaced74eff089b2fc
Quynh Nguyen <46946420+quynhng336@users.noreply.github.com>
2022-07-13 07:21:59 -0700

    added home.js

---
+++ home.js
@@ -0,0 +1,22 @@
+let day = 2;
+switch (day) {
+    case 1:
+        console.log("1 days until Saturday");
+        break;
+    case 2: 
+        console.log("2 days until Saturday");
+        break;
+    case 3: 
+        console.log("3 days until Saturday");
+        break;
+    case 4: 
+        console.log("4 days until Saturday");
+        break;
+    case 5: 
+        console.log("5 days until Saturday");
+        break;
+    case 6: 
+        console.log("6 days until Saturday");
+        break;
+
+}
